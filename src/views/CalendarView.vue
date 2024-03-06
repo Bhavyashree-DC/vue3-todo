64ee3302ac4d6b0a77c10c7119449fe9b0d709d0
Bhavyashree_DC <bhavyadc05@gmail.com>
2024-03-06 10:11:33 +0530

    Added router for NavBar

---
+++ src/views/CalendarView.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+      <el-calendar v-model="value" />
+      <router-view></router-view>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  const value = ref(new Date());
+  </script>
